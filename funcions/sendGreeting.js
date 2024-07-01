b0281a4c409e3e798962e8c0c676ee3b3308ebed
divineamunega <divineamunega@gmail.com>
2024-07-01 14:08:37 +0100

    modified:   funcions/sendGreeting.js

--- funcions/sendGreeting.js
+++ funcions/sendGreeting.js
@@ -1,5 +1,12 @@
 export default (req, res, next) => {
 	const name = req.query?.visitor_name;
-	console.log(req.socket.remoteAddress);
+	const clinetIp = req.socket.remoteAddress;
 	if (!name) next(new Error("Please Input a visitor_name parameter"));
+
+	res
+		.status(200)
+		.json({
+			message: "success",
+			data: { clinetIp, req: req.headers["x-real-ip"] },
+		});
 };
