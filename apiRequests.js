ecbfbad94b7168254a06643ab054d327baeecf1b
divineamunega <divineamunega@gmail.com>
2024-07-01 15:22:05 +0100

    modified:   apiRequests.js 	modified:   funcions/sendGreeting.js

--- apiRequests.js
+++ apiRequests.js
@@ -1,9 +1,16 @@
-const API_URL = "http://ip-api.com/json";
-const fetchLocationFromIP = async function (ip) {
-	const res = await fetch(`${API_URL}/${ip}`);
+//  https://api.weatherapi.com/v1/current.json?key=c7415bbfabab4dcc8c8140304240107&q=London&aqi=no
 
-	const data = await res.json();
-	console.log(data);
+const API_URL = "https://api.weatherapi.com/v1/current.json";
+const API_KEY = "c7415bbfabab4dcc8c8140304240107";
+
+const fetchWeatherFromIP = async function (ip) {
+	try {
+		const res = await fetch(`${API_URL}?key=${API_KEY}&q=${ip}&aqi=no`);
+		const data = await res.json();
+		return data;
+	} catch (err) {
+		next(err);
+	}
 };
 
-export { fetchLocationFromIP };
+export { fetchWeatherFromIP };
--- funcions/sendGreeting.js
+++ funcions/sendGreeting.js
@@ -1,12 +1,21 @@
-export default (req, res, next) => {
+import { fetchWeatherFromIP } from "../apiRequests.js";
+
+export default async (req, res, next) => {
 	const name = req.query?.visitor_name;
-	const clinetIp = req.socket.remoteAddress;
-	if (!name) next(new Error("Please Input a visitor_name parameter"));
+	const client_ip =
+		req.headers["x-real-ip"] || req.socket.remoteAddress.split(7);
 
-	console.log(req.headers);
-
-	res.status(200).json({
-		message: "success",
-		data: { clinetIp, req: req.headers },
-	});
+	if (!name) next(new Error("Please input a visitor_name parameter"));
+	try {
+		const data = await fetchWeatherFromIP(client_ip);
+		const location = data.location.region;
+		const greeting = `Hello, Mark!, the temperature is ${data.current.temp_c}  degrees Celsius in ${location}`;
+		res.status(200).json({
+			client_ip,
+			location,
+			greeting,
+		});
+	} catch (err) {
+		next(err);
+	}
 };
